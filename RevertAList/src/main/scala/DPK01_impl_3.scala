c2b70d7b46ce2f48109f00eacdf37579737b271b
Pedro <pedrohk@gmail.com>
2025-11-11 00:23:11 -0300

    more implementation and fixing code

--- RevertAList/src/main/scala/DPK01_impl_3.scala
+++ RevertAList/src/main/scala/DPK01_impl_3.scala
@@ -1,11 +1,11 @@
 object DPK01_impl_3 {
   def revert(list: List[Int]): List[Int] = {
-    val arr = list.toArray
-    var i = arr.length - 1
     var res: List[Int] = Nil
-    while i >= 0 do {
-      res =  res
-      i -= 1
+    var i = 0
+    val arr = list.toArray
+    while (i < arr.length) {
+      res = arr(i) :: res
+      i += 1
     }
     res
   }
--- RevertAList/src/test/scala/RevertListTest.scala
+++ RevertAList/src/test/scala/RevertListTest.scala
@@ -4,16 +4,29 @@ class RevertListTest extends AnyFunSuite {
   val input = List(1,2,3,4,5)
   val expected = List(5,4,3,2,1)
 
-  test("revert1") {
+  test("DPK01_impl_1") {
     assert(DPK01_impl_1.revert(input) == expected)
   }
 
-  test("revert2") {
+  test("DPK01_impl_2") {
     assert(DPK01_impl_2.revert(input) == expected)
   }
 
-  test("revert3") {
+  test("DPK01_impl_3") {
     assert(DPK01_impl_3.revert(input) == expected)
   }
+
+  test("DPK01_impl_4") {
+    assert(DPK01_impl_4.revert(input) == expected)
+  }
+
+  test("DPK01_impl_5") {
+    assert(DPK01_impl_5.revert(input) == expected)
+  }
+
+  test("DPK01_impl_6") {
+    assert(DPK01_impl_6.revert(input) == expected)
+  }
+
 
 }
