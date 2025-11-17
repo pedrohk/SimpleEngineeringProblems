f3a5e4eecf4ff1497faf4761a5770c31829063ae
Pedro <pedrohk@gmail.com>
2025-11-17 00:16:46 -0300

    lookup scala

--- lookUpScala/src/test/scala/LookupScalaTests.scala
+++ lookUpScala/src/test/scala/LookupScalaTests.scala
@@ -4,5 +4,6 @@ class LookupTests extends AnyFunSuite {
 
   test("lookup implementations return John for key 1") {
     assert(DPK01_impl_1.lookup(1) == "John")
+    assert(DPK01_impl_2.lookup(1) == "John")
   }
 }
