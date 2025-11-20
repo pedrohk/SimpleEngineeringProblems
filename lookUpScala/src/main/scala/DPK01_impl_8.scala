0eeeb90dfc1d29e21a09824bdee55a1e4f01c8e7
Pedro <pedrohk@gmail.com>
2025-11-20 01:29:03 -0300

    lookup scala

---
+++ lookUpScala/src/main/scala/DPK01_impl_8.scala
@@ -0,0 +1,14 @@
+object DPK01_impl_8 {
+  val data = List((1, "John"), (2, "Alice"), (3, "Bob"))
+  def lookup(k: Int): String = {
+    var r = ""
+    var xs = data
+    var i = 0
+    while i < data.length do {
+      if xs.head._1 == k then r = xs.head._2
+      xs = xs.tail
+      i = i + 1
+    }
+    r
+  }
+}
--- lookUpScala/src/test/scala/LookupScalaTests.scala
+++ lookUpScala/src/test/scala/LookupScalaTests.scala
@@ -9,5 +9,7 @@ class LookupTests extends AnyFunSuite {
     assert(DPK01_impl_4.lookup(1) == "John")
     assert(DPK01_impl_5.lookup(1) == "John")
     assert(DPK01_impl_6.lookup(1) == "John")
+    assert(DPK01_impl_7.lookup(2) == "Alice")
+    assert(DPK01_impl_8.lookup(3) == "Bob")
   }
 }
