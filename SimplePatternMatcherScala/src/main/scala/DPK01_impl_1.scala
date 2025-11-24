2ac1e4efe8017dd1a0467a6c632942099bb9d5ca
Pedro <pedrohk@gmail.com>
2025-11-24 01:08:48 -0300

    SimplePatternMatcher Scala adding new

---
+++ SimplePatternMatcherScala/src/main/scala/DPK01_impl_1.scala
@@ -0,0 +1,13 @@
+object DPK01_impl_1 {
+  def pattern_matcher(c: String): String = {
+    c match {
+      case "Usa" => "English"
+      case "Brazil" => "Portuguese"
+      case "Spain" => "Spanish"
+      case "Italy" => "Italian"
+      case "France" => "French"
+      case "Germany" => "German"
+      case _ => ""
+    }
+  }
+}
---
+++ SimplePatternMatcherScala/src/test/scala/SimplePatternMatcherTests.scala
@@ -0,0 +1,27 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+class SimplePatternMatcherTests extends AnyFunSuite {
+  val data = Map(
+    "Usa" -> "English",
+    "Brazil" -> "Portuguese",
+    "Spain" -> "Spanish",
+    "Italy" -> "Italian",
+    "France" -> "French",
+    "Germany" -> "German"
+  )
+
+  val impls = Seq(
+    DPK01_impl_1.pattern_matcher,
+    DPK01_impl_2.pattern_matcher
+  )
+
+  test("all implementations return correct result") {
+    for (impl <- impls) {
+      for ((c, l) <- data) {
+        assert(impl(c) == l)
+      }
+      assert(impl("Unknown") == "")
+    }
+  }
+
+}
