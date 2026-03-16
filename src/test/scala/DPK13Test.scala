68ec8ec6b64f377a1b3108820c8aa6c6a437eb1b
Pedro <pedrohk@gmail.com>
2026-03-16 01:14:45 -0300

    DPK12 new

---
+++ src/test/scala/DPK13Test.scala
@@ -0,0 +1,13 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+class DPK13Test extends AnyFunSuite {
+
+  val expected10 = Array[Any](
+    1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz"
+  )
+
+  test("DPK13_impl_01") {
+    assert(DPK13_impl_01.fizzbuzz(10).sameElements(expected10))
+  }
+
+}
