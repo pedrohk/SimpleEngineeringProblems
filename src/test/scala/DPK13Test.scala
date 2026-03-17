077d4093dc2f00933456c383501324b7f61464f9
Pedro <pedrohk@gmail.com>
2026-03-17 01:57:32 -0300

    DPK13 new

--- src/test/scala/DPK13Test.scala
+++ src/test/scala/DPK13Test.scala
@@ -22,4 +22,8 @@ class DPK13Test extends AnyFunSuite {
     assert(DPK13_impl_04.fizzbuzz(10).sameElements(expected10))
   }
 
+  test("DPK13_impl_05") {
+    assert(DPK13_impl_05.fizzbuzz(10).sameElements(expected10))
+  }
+
 }
