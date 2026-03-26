0d62e5f44a1bae12619328bdd1a19c6346640971
Pedro <pedrohk@gmail.com>
2026-03-26 02:13:35 -0300

    DPK15 new

---
+++ src/main/scala/DPK15_impl_06.scala
@@ -0,0 +1,48 @@
+import scala.collection.mutable.ArrayBuffer
+
+object DPK15_impl_06 {
+
+  class Friends {
+    private val buffer = ArrayBuffer[Person]()
+
+    def add(friend: Person): Unit = {
+      if (!buffer.exists(_.getName() == friend.getName())) {
+        buffer += friend
+      }
+    }
+
+    def remove(friend: Person): Unit = {
+      val idx = buffer.indexWhere(_.getName() == friend.getName())
+      if (idx >= 0) buffer.remove(idx)
+    }
+
+    def all(): List[Person] = buffer.toList
+  }
+
+  class Person(private val name: String, private val age: Int) extends PersonOps {
+    private val friends = new Friends()
+
+    def getName(): String = name
+
+    def getAge(): Int = age
+
+    def addFriend(friend: PersonOps): Unit =
+      friends.add(friend.asInstanceOf[Person])
+
+    def removeFriend(friend: PersonOps): Unit =
+      friends.remove(friend.asInstanceOf[Person])
+
+    def getFriends(): List[PersonOps] = friends.all()
+  }
+
+  object SocialAnalytics {
+    def mostFriends(persons: List[Person]): Person =
+      persons.maxBy(_.getFriends().size)
+
+    def leastFriends(persons: List[Person]): Person =
+      persons.minBy(_.getFriends().size)
+
+    def oldestFriend(persons: List[Person]): Person =
+      persons.flatMap(_.getFriends()).map(_.asInstanceOf[Person]).maxBy(_.getAge())
+  }
+}
