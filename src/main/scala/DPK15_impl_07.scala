76b34836225c5f9805b4954c235e0150b47b4978
Pedro <pedrohk@gmail.com>
2026-03-27 00:33:32 -0300

    DPK15 new

---
+++ src/main/scala/DPK15_impl_07.scala
@@ -0,0 +1,41 @@
+import scala.collection.mutable.LinkedHashSet
+
+object DPK15_impl_07 {
+
+  class Friends {
+    private val set = LinkedHashSet[Person]()
+
+    def add(friend: Person): Unit = set += friend
+
+    def remove(friend: Person): Unit = set -= friend
+
+    def all(): List[Person] = set.toList
+  }
+
+  class Person(private val name: String, private val age: Int) extends PersonOps {
+    private val friends = new Friends()
+
+    def getName(): String = name
+
+    def getAge(): Int = age
+
+    def addFriend(friend: PersonOps): Unit =
+      friends.add(friend.asInstanceOf[Person])
+
+    def removeFriend(friend: PersonOps): Unit =
+      friends.remove(friend.asInstanceOf[Person])
+
+    def getFriends(): List[PersonOps] = friends.all()
+  }
+
+  object SocialAnalytics {
+    def mostFriends(persons: List[Person]): Person =
+      persons.maxBy(_.getFriends().size)
+
+    def leastFriends(persons: List[Person]): Person =
+      persons.minBy(_.getFriends().size)
+
+    def oldestFriend(persons: List[Person]): Person =
+      persons.flatMap(_.getFriends()).map(_.asInstanceOf[Person]).maxBy(_.getAge())
+  }
+}
