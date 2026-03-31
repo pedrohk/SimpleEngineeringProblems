c4e73dff10a621e84c15733e8a3e4a052f9ba23b
Pedro <pedrohk@gmail.com>
2026-03-31 18:00:30 -0300

    DPK16 new

---
+++ src/main/scala/DPK16_impl_07.scala
@@ -0,0 +1,50 @@
+object DPK16_impl_07 {
+
+  case class Position(var x: Int, var y: Int)
+
+  class Mosquito(var pos: Position) {
+    var alive = true
+
+    def move(size: Int): Unit = {
+      pos.x = (pos.x + 1) % size
+      pos.y = (pos.y + 1) % size
+    }
+  }
+
+  class Exterminator(var pos: Position) {
+    def move(size: Int): Unit = {
+      pos.x = (pos.x + 2) % size
+      pos.y = (pos.y + 2) % size
+    }
+  }
+
+  class Game {
+    val size = 100
+    var killed = 0
+
+    val exterminator = new Exterminator(Position(0, 0))
+    var mosquitos = scala.collection.mutable.ListBuffer.fill(10)(
+      new Mosquito(Position(scala.util.Random.nextInt(size), scala.util.Random.nextInt(size)))
+    )
+
+    def tick(): Unit = {
+      exterminator.move(size)
+
+      mosquitos.foreach { m =>
+        if (m.alive) {
+          m.move(size)
+          if (m.pos == exterminator.pos) {
+            m.alive = false; killed += 1
+          }
+        }
+      }
+
+      mosquitos = mosquitos.filter(_.alive)
+    }
+
+    def aliveCount = mosquitos.size
+
+    def killedCount = killed
+  }
+
+}
--- src/test/scala/DPK16Test.scala
+++ src/test/scala/DPK16Test.scala
@@ -36,4 +36,8 @@ class DPK16Test extends AnyFunSuite {
   test("DPK16_impl_06") {
     runGame(new DPK16_impl_06.Game)
   }
+
+  test("DPK16_impl_07") {
+    runGame(new DPK16_impl_07.Game)
+  }
 }
